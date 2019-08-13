29e981d21c1df1dfb3a7d5fb2b233b175be6d8e0
Andy Gimma <andy@hylo.com>
2019-08-13 15:22:12 +0300

    Remove blog

--- db/schema.rb
+++ db/schema.rb
@@ -15,11 +15,4 @@ ActiveRecord::Schema.define(version: 2018_12_15_080306) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "blogs", force: :cascade do |t|
-    t.string "title"
-    t.string "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
 end
