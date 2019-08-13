8376f7d7f1e49371196b9eec9774ad39c5466ba6
Andy Gimma <andy@hylo.com>
2019-08-14 01:30:51 +0300

    Customize scaffolding by removing the users ability to add user_id or read? boolean to email

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -1,5 +1,6 @@
 class EmailsController < ApplicationController
   before_action :set_email, only: [:show, :edit, :update, :destroy]
+  before_action :require_login
 
   # GET /emails
   # GET /emails.json
@@ -25,7 +26,7 @@ class EmailsController < ApplicationController
   # POST /emails.json
   def create
     @email = Email.new(email_params)
-
+    @email.user = current_user
     respond_to do |format|
       if @email.save
         format.html { redirect_to @email, notice: 'Email was successfully created.' }
@@ -69,6 +70,6 @@ class EmailsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def email_params
-      params.require(:email).permit(:user_id, :subject, :body, :read)
+      params.require(:email).permit(:subject, :body)
     end
 end
