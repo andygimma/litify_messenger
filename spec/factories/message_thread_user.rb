f3c1974496aebd59cfdad3f83be7fb69dd78fd7d
andy gimma <andrewgimma@protonmail.com>
2019-08-18 18:42:38 +0000

    Add message_thread_user factory

---
+++ spec/factories/message_thread_user.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :message_thread_user do
+    user_id { FactoryBot.create(:user).id }
+    message_thread_id { FactoryBot.create(:message_thread).id }
+    email_id { FactoryBot.create(:email).id }
+  end
+end
