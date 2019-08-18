e0bfc70a40457e94418a394321612f97182c1fb1
andy gimma <andrewgimma@protonmail.com>
2019-08-18 18:02:01 +0000

    Add TODO

--- app/controllers/message_threads_controller.rb
+++ app/controllers/message_threads_controller.rb
@@ -2,6 +2,7 @@ class MessageThreadsController < ApplicationController
   before_action :require_login
 
   def show
+    # TODO ensure permission
     @emails = MessageThread.find(params[:id])
                 .emails
                 .paginate(page: params[:page], per_page: 10)
