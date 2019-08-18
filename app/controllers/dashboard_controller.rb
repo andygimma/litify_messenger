6ebb4fb3e5ef77e7138f6bbf8e9492641d5bd513
andy gimma <andrewgimma@protonmail.com>
2019-08-18 19:45:46 +0000

    Show one email per thread

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,17 +2,9 @@ class DashboardController < ApplicationController
   before_action :require_login
 
   def home
-    # @threads = MessageThread
-    #             .where(user_id: current_user.id)
-    #             .order(id: :desc)                
-    #             .preload(:emails)
-    #             .paginate(page: params[:page], per_page: 10)
-
-    @threads = MessageThread.where(
-                id: MessageThreadUser
-                  .where(user_id: current_user.id)
-                  .pluck(:message_thread_id)
-              )
-              .paginate(page: params[:page], per_page: 10)
+    @threads = MessageThreadUser
+                .select('DISTINCT ON ("message_thread_id") *')
+                .where(user_id: current_user.id)
+                .paginate(page: params[:page], per_page: 10)
   end
 end
