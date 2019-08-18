b05d495f91823170ba54b08d6ce3ce8f13bdddf1
andy gimma <andrewgimma@protonmail.com>
2019-08-18 16:50:16 +0000

    Show threads on dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,14 +2,10 @@ class DashboardController < ApplicationController
   before_action :require_login
 
   def home
-    # @my_message_thread_users = MessageThreadUser.where(user_id: current_user.id).threads.emails
-    @emails = Email
-                .where(message_thread_id:
-                  MessageThreadUser
-                    .where(user_id: current_user.id)
-                    .pluck(:message_thread_id)
-              )
-              .order(created_at: :desc)
-              .paginate(page: params[:page], per_page: 10)
+    @threads = MessageThread
+                .where(user_id: current_user.id)
+                .order(id: :desc)                
+                .preload(:emails)
+                .paginate(page: params[:page], per_page: 10)
   end
 end
