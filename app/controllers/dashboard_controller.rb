38cdfd9e91084a96be9fa2ae754f58bf3059e423
andy gimma <andrewgimma@protonmail.com>
2019-08-19 11:17:37 +0000

    Finalize message_thread_user list function

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,8 +3,9 @@ class DashboardController < ApplicationController
 
   def home
     @threads = MessageThreadUser
-                .select('DISTINCT ON ("message_thread_id") *')
                 .where(user_id: current_user.id)
+                .order('message_thread_users.message_thread_id, created_at DESC')
+                .select('DISTINCT ON ("message_thread_id") *')
                 .paginate(page: params[:page], per_page: 10)
   end
 end
